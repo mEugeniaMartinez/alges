6fb4f6f8f7c207e5bbfdae3dfa0785ba95995cb3
Mariu <100346843@alumnos.uc3m.es>
2022-06-06 18:24:33 +0200

    change deliverynote schema

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -19,7 +19,6 @@ class BaseController extends AbstractController
 
     public function __construct(EntityManagerInterface $entityManager)
     {
-
         $this->em = $entityManager;
     }
 
@@ -27,26 +26,26 @@ class BaseController extends AbstractController
     public function index(): Response
     {
 
-        $userRepo = $this->em->getRepository(User::class);
+        /*$userRepo = $this->em->getRepository(User::class);
         $userDB = $userRepo->find(26);
-        $dnRepo = $this->em->getRepository(DeliveryNote::class);
-        $dnDB = $dnRepo->find(1);
-        $date = date_create_from_format('Y-m-d', '2022-06-30');
-        $dnDB->setDate($date);
+        $clientRepo = $this->em->getRepository(Client::class);
+        $clientDB = $clientRepo->find(27);
+        $date = date_create_from_format('d-m-Y', date("d-m-Y"));
+
+        $dn = new DeliveryNote($userDB);
+        $dn->setClient($clientDB);
+        $dn->setDate($date);
+        $this->em->persist($dn);
         $this->em->flush();
 
-        /*$dn = new DeliveryNote("notSigned", $userDB);
-        $dn->setClient($clientDB);
-        $dn->getDate(date("Y-m-d"));
-
-        $this->em->persist($dn);
-        $this->em->flush();*/
+        $dn->setNumber($dn->getId());
+        $this->em->flush();
 
         $dns = $userDB->getDeliveryNotes();
         foreach ($dns as $d) {
-            var_dump($d->getClient()->getEmail());
+            var_dump($d->getNumber());
             var_dump($d->getDate()->format('d-m-Y'));
-        }
+        }*/
 
         return $this->render('base.html.twig', [
             'controller_name' => 'BaseController',
--- src/Entity/DeliveryNote.php
+++ src/Entity/DeliveryNote.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use App\Repository\DeliveryNoteRepository;
 use Doctrine\ORM\Mapping as ORM;
+use phpDocumentor\Reflection\DocBlock\Tags\Return_;
+use Symfony\Component\Validator\Constraints\Date;
 
 #[ORM\Entity(repositoryClass: DeliveryNoteRepository::class)]
 class DeliveryNote
@@ -34,9 +36,6 @@ class DeliveryNote
     #[ORM\Column(type: 'blob', nullable: true)]
     private $pdf;
 
-    #[ORM\Column(type: 'string', length: 255)]
-    private $state;
-
     #[ORM\ManyToOne(targetEntity: Client::class, inversedBy: 'deliveryNotes')]
     private $client;
 
@@ -44,16 +43,25 @@ class DeliveryNote
     #[ORM\JoinColumn(nullable: false)]
     private $user;
 
-    /**
-     * @param $state
-     * @param $user
-     */
-    public function __construct(string $state, User $user)
-    {
-        $this->state = $state;
-        $this->user = $user;
-    }
+    #[ORM\Column(type: 'boolean')]
+    private $signed;
 
+    #[ORM\Column(type: 'boolean')]
+    private $completed;
+
+    #[ORM\Column(type: 'boolean')]
+    private $disabled;
+
+    /**
+     * @param User $user
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+        $this->signed = false;
+        $this->disabled = false;
+        $this->completed = false;
+    }
 
     public function getId(): ?int
     {
@@ -113,9 +121,9 @@ class DeliveryNote
         return $this->number;
     }
 
-    public function setNumber(?string $number): self
+    public function setNumber(?string $id): self
     {
-        $this->number = $number;
+        $this->number = $this->generateNumber($id);
 
         return $this;
     }
@@ -144,18 +152,6 @@ class DeliveryNote
         return $this;
     }
 
-    public function getState(): ?string
-    {
-        return $this->state;
-    }
-
-    public function setState(string $state): self
-    {
-        $this->state = $state;
-
-        return $this;
-    }
-
     public function getClient(): ?Client
     {
         return $this->client;
@@ -178,5 +174,48 @@ class DeliveryNote
         $this->user = $user;
 
         return $this;
+    }
+
+    public function isSigned(): ?bool
+    {
+        return $this->signed;
+    }
+
+    public function setSigned(bool $signed): self
+    {
+        $this->signed = $signed;
+
+        return $this;
+    }
+
+    public function isCompleted(): ?bool
+    {
+        return $this->completed;
+    }
+
+    public function setCompleted(bool $completed): self
+    {
+        $this->completed = $completed;
+
+        return $this;
+    }
+
+    public function isDisabled(): ?bool
+    {
+        return $this->disabled;
+    }
+
+    public function setDisabled(bool $disabled): self
+    {
+        $this->disabled = $disabled;
+
+        return $this;
+    }
+
+    private function generateNumber(?string $id): string
+    {
+        $date = date("my", strtotime("today"));; // 06 Jun, 2022 -> 0622
+        return 'AL_' . str_pad($id . $date, 7,
+            "0", STR_PAD_LEFT); //AL_0020622
     }
 }
